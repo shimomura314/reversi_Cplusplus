2b02e8856580a060cf302733a1b3ffcaf21a3b25
shimomura314 <yukishimomura314@gmail.com>
2024-04-20 03:05:27 +0900

    otw

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 # mingw32-make
 
 # コンパイラの定義・コンパイルオプション
-CC     = g++
-CFLAGS = -Wall -Wextra -Werror
+CC     = emcc
+CFLAGS = -Wall -Wextra -Werror -s WASM=1
 
 # 最終ターゲットファイル
 all: othello
@@ -11,7 +11,7 @@ all: othello
 SRCS_othello   = $(wildcard bitboard/*.cpp strategy/*.cpp)
 OBJS_othello   = $(SRCS_othello:.cpp=.o)
 othello: $(OBJS_othello)
-	$(CC) -o $@ $^ $(LIBDIR) $(LIBS)
+	$(CC) -o $@ $^ $(LIBDIR) $(LIBS) -s 'INVOKE_RUN=0' -s EXPORTED_FUNCTIONS='["_playGame","_initGame"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]'
 
 # 汎用的な.cppから.oへの変換ルール
 INCDIR  =  -Ibitboard -Istrategy
---
+++ bitboard/c2js.cpp
@@ -0,0 +1,78 @@
+#include "othello.h"
+
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(my_module) {
+    emscripten::class_<BitBoard>("BitBoard")
+        .constructor()
+    // py::class_<BitBoard>(m, "BitBoard")
+    //     .def(py::init<>())
+
+    //     .def_static("bit_count", &BitBoard::bit_count, py::arg("x"))
+    //     .def_static(
+    //         "check_surround", 
+    //         &BitBoard::check_surround, py::arg("x"), py::arg("direction"))
+    //     .def_static(
+    //         "count_disks", &BitBoard::count_disks, py::arg("boards"))
+    //     .def_static(
+    //         "reversible_area", &BitBoard::reversible_area,
+    //         py::arg("turn"), py::arg("boards"))
+    //     .def_static(
+    //         "reversible_area_list", &BitBoard::reversible_area_list,
+    //         py::arg("turn"), py::arg("boards"))
+    //     .def_static(
+    //         "is_reversible", &BitBoard::is_reversible,
+    //         py::arg("put_loc"), py::arg("turn"), py::arg("boards"))
+    //     .def_static(
+    //         "turn_playable", &BitBoard::turn_playable,
+    //         py::arg("turn"), py::arg("boards"))
+    //     .def_static(
+    //         "judge_game", &BitBoard::judge_game, py::arg("boards"))
+
+    //     .def("return_board", &BitBoard::return_board)
+    //     .def("return_turn", &BitBoard::return_turn)
+    //     .def("return_result", &BitBoard::return_result)
+    //     .def("display_board", &BitBoard::display_board)
+    //     .def(
+    //         "load_board", &BitBoard::load_board,
+    //         py::arg("boards"))
+    //     .def("load_state", &BitBoard::load_state)
+    //     .def("save_state", &BitBoard::save_state)
+    //     .def("undo_turn", &BitBoard::undo_turn)
+    //     .def("redo_turn", &BitBoard::redo_turn)
+
+    //     .def_static(
+    //         "simulate_play", &BitBoard::simulate_play,
+    //         py::arg("turn"), py::arg("put_loc"), py::arg("boards"))
+    //     .def("play_turn", &BitBoard::play_turn, py::arg("put_loc"))
+    //     .def("skip_turn", &BitBoard::skip_turn)
+    //     .def("update_count", &BitBoard::update_count)
+    //     .def("auto_mode", &BitBoard::auto_mode, py::arg("automode") = true)
+    //     ;
+
+    // py::class_<BitBoard::Boards>(m, "MyStruct")
+    //     .def_readwrite("black", &BitBoard::Boards::black)
+    //     .def_readwrite("white", &BitBoard::Boards::white)
+    //     ;
+
+    // // Strategyクラスのバインディング
+    // py::class_<Strategy>(m, "Strategy")
+    //     .def(py::init<const std::string&>(), py::arg("strategy") = "random")
+    //     .def("set_strategy", &Strategy::set_strategy, py::arg("strategy"))
+    //     .def("move", &Strategy::move, py::arg("board"))
+    //     ;
+
+    // // Othelloクラスのバインディング
+    // py::class_<Othello, BitBoard>(m, "Othello") // BitBoardを基本クラスとして指定
+    //     .def(py::init<uint8_t>(), py::arg("color") = 0)
+    //     .def("return_player_clr", &Othello::return_player_clr)
+    //     .def(
+    //         "change_strategy", &Othello::change_strategy,
+    //         py::arg("strategy"), py::arg("is_player") = false)
+    //     .def("change_player", &Othello::change_player, py::arg("color"))
+    //     .def("process_game", &Othello::process_game)
+    //     // 他の必要なメンバ関数をここにバインド
+    ;
+}
